cef5469b99b98d70d206e14e61e10fa52b9a5f5e
Garima1996Sharma <96504577+Garima1996Sharma@users.noreply.github.com>
2021-12-21 16:23:37 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+//////////////////////////  picuture /////////////////////////
+
+function overEffect(image) {
+    console.log(image);
+    image.src = "Images/red_wolf.jfif"
+
+}
+
+function outEffect() {
+    images = document.getElementsByTagName("img");
+    console.log(images);
+    console.log(images[0]);
+    images[0].src = "Images/redd_wolf.jfif"
+}
+//////////////////////////  picuture 1 /////////////////////////
+function overEffect1(image) {
+    console.log(image);
+    image.src = "Images/siberian1.jfif"
+
+}
+
+function outEffect1() {
+    images = document.getElementsByTagName("img");
+    console.log(images);
+    console.log(images[0]);
+    images[1].src = "Images/siberian.jpg"
+}
+
+//////////////////////////  picuture 2 /////////////////////////
+function overEffect2(image) {
+    console.log(image);
+    image.src = "Images/lion1.jfif"
+
+}
+
+function outEffect2() {
+    images = document.getElementsByTagName("img");
+    console.log(images);
+    console.log(images[0]);
+    images[2].src = "Images/lion.jfif"
+}
+//////////////////////////  picuture 3 /////////////////////////
+function overEffect3(image) {
+    console.log(image);
+    image.src = "Images/orangutan1.jfif"
+
+}
+
+function outEffect3() {
+    images = document.getElementsByTagName("img");
+    console.log(images);
+    console.log(images[0]);
+    images[3].src = "Images/orangutan.jfif"
+}
+//////////////////////////  picuture 4 /////////////////////////
+function overEffect4(image) {
+    console.log(image);
+    image.src = "Images/panda1.jfif"
+
+}
+
+function outEffect4() {
+    images = document.getElementsByTagName("img");
+    console.log(images);
+    console.log(images[0]);
+    images[4].src = "Images/panda.jfif"
+}
